4ab08871261f8856a02d840111535230a74a2e32
Philipp Hoffmann <mail@philipphoffmann.de>
2012-08-04 19:39:57 +0200

    refactored settings dialog

--- prefs.js
+++ prefs.js
@@ -18,22 +18,6 @@ function init() {
     settings = convenience.getSettings(extension, SETTINGS_SCHEMA);
 }
 
-function changedJenkinsUrl(input) {
-	settings.set_string("jenkins-url", input.text);
-}
-
-function changedAutoRefresh(input) {
-	settings.set_boolean("autorefresh", input.get_active());
-}
-
-function changedAutorefreshInterval(input) {
-	settings.set_int("autorefresh-interval", input.text);
-}
-
-function changedFilterDisabledJobs(input) {
-	settings.set_boolean("filter-disabled-jobs", input.get_active());
-}
-
 function buildPrefsWidget() {
     let frame = new Gtk.Box({ orientation: Gtk.Orientation.VERTICAL,
                               border_width: 10 });
@@ -41,62 +25,61 @@ function buildPrefsWidget() {
     let vbox = new Gtk.Box({ orientation: Gtk.Orientation.VERTICAL,
                              margin_left: 20 });
     
+    // *** jenkins connection ***
     let labelJenkinsConnection = new Gtk.Label({ label: "<b>" + _("Jenkins connection") + "</b>", use_markup: true, xalign: 0 });
+    vbox.add(labelJenkinsConnection);
 
 	// jenkins url
     let hboxJenkinsUrl = new Gtk.Box({orientation: Gtk.Orientation.HORIZONTAL});
 	let labelJenkinsUrl = new Gtk.Label({label: _("Jenkins CI Server web frontend URL"), xalign: 0});
 	let inputJenkinsUrl = new Gtk.Entry({ hexpand: true, text: settings.get_string("jenkins-url") });
 	
-	inputJenkinsUrl.connect("changed", Lang.bind(this, changedJenkinsUrl));
+	inputJenkinsUrl.connect("changed", Lang.bind(this, function(input){	settings.set_string("jenkins-url", input.text); }));
 
     hboxJenkinsUrl.pack_start(labelJenkinsUrl, true, true, 0);
 	hboxJenkinsUrl.add(inputJenkinsUrl);
+	vbox.add(hboxJenkinsUrl);
 	
 	
+	// *** preferences ***
 	let labelPreferences = new Gtk.Label({ label: "<b>" + _("preferences") + "</b>", use_markup: true, xalign: 0 });
+	vbox.add(labelPreferences);
 	
 	// auto refresh
 	let hboxAutoRefresh = new Gtk.Box({orientation: Gtk.Orientation.HORIZONTAL});
 	let labelAutoRefresh = new Gtk.Label({label: _("auto-refresh"), xalign: 0});
 	let inputAutoRefresh = new Gtk.Switch({active: settings.get_boolean("autorefresh")});
 
-	inputAutoRefresh.connect("notify::active", Lang.bind(this, changedAutoRefresh));
+	inputAutoRefresh.connect("notify::active", Lang.bind(this, function(input){	settings.set_boolean("autorefresh", input.get_active()); }));
 
     hboxAutoRefresh.pack_start(labelAutoRefresh, true, true, 0);
 	hboxAutoRefresh.add(inputAutoRefresh);
+	vbox.add(hboxAutoRefresh);
 	
 	// auto refresh interval
 	let hboxAutorefreshInterval = new Gtk.Box({orientation: Gtk.Orientation.HORIZONTAL});
 	let labelAutorefreshInterval = new Gtk.Label({label: _("auto-refresh interval (seconds)"), xalign: 0});
 	let inputAutorefreshInterval = new Gtk.Entry({ hexpand: true, text: ""+settings.get_int("autorefresh-interval") });
 	
-	inputAutorefreshInterval.connect("changed", Lang.bind(this, changedAutorefreshInterval));
+	inputAutorefreshInterval.connect("changed", Lang.bind(this, function(input){ settings.set_int("autorefresh-interval", input.text); }));
 
     hboxAutorefreshInterval.pack_start(labelAutorefreshInterval, true, true, 0);
 	hboxAutorefreshInterval.add(inputAutorefreshInterval);
+	vbox.add(hboxAutorefreshInterval);
 	
 	// filter disables jobs
 	let hboxFilterDisabledJobs = new Gtk.Box({orientation: Gtk.Orientation.HORIZONTAL});
 	let labelFilterDisabledJobs = new Gtk.Label({label: _("filter disabled jobs"), xalign: 0});
 	let inputFilterDisabledJobs = new Gtk.Switch({active: settings.get_boolean("filter-disabled-jobs")});
 
-	inputFilterDisabledJobs.connect("notify::active", Lang.bind(this, changedFilterDisabledJobs));
+	inputFilterDisabledJobs.connect("notify::active", Lang.bind(this, function(input){settings.set_boolean("filter-disabled-jobs", input.get_active()); }));
 
     hboxFilterDisabledJobs.pack_start(labelFilterDisabledJobs, true, true, 0);
 	hboxFilterDisabledJobs.add(inputFilterDisabledJobs);
-	
-	
-	vbox.add(labelJenkinsConnection)
-	vbox.add(hboxJenkinsUrl);
-	
-	vbox.add(labelPreferences)
-	vbox.add(hboxAutoRefresh);
-	vbox.add(hboxAutorefreshInterval);
 	vbox.add(hboxFilterDisabledJobs);
-	
-	frame.add(vbox);
-    
+
+
+	frame.add(vbox);    
     frame.show_all();
     
     return frame;
